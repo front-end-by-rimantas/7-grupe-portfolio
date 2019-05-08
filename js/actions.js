375a69ceb20af2747bd7a9772d5b0d6a1b3d434c
Jonas Chairutdinovas <jonas.chairutdinovas@gmail.com>
2019-05-08 16:30:27 +0300

    add testimonials generation

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,6 @@
 "use strict";
 
 document.querySelector('#services .service-list').innerHTML = generateServices( services );
+
+
+document.querySelector('#testimonials .testimonial').innerHTML = generateTestimonial(testimonials);
