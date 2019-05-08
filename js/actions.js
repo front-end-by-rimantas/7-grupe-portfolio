855d193384f05559e1db77078a9cd333c3796998
Jonas Chairutdinovas <jonas.chairutdinovas@gmail.com>
2019-05-08 16:30:47 +0300

    Revert "add testimonials generation"
    This reverts commit 375a69ceb20af2747bd7a9772d5b0d6a1b3d434c.

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,3 @@
 "use strict";
 
 document.querySelector('#services .service-list').innerHTML = generateServices( services );
-
-
-document.querySelector('#testimonials .testimonial').innerHTML = generateTestimonial(testimonials);
--- js/function.js
+++ js/function.js
@@ -18,20 +18,3 @@ function generateServices( data ) {
 
     return HTML;
 }
-
-function generateTestimonial(data){
-    var HTML = '';
-
-    if (!Array.isArray(data)){
-        return HTML;
-    }
-
-    if (data.length > 0){
-        var i=2;
-        HTML += '<img src='+data[i].avatar+' alt= '+data[i].name+'>\
-                <p>'+data[i].p+'</p>\
-                <h4>'+data[i].name+'</h4>\
-                <p>'+data[i].position+'</p>';
-    }
-    return HTML;
-}
