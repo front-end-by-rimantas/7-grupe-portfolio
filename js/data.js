0fa7c81b17cea15c69ccc1c9e49e91415eeac3ac
Jonas Chairutdinovas <jonas.chairutdinovas@gmail.com>
2019-05-08 16:35:14 +0300

    add testimonials struct #8

--- js/data.js
+++ js/data.js
@@ -17,3 +17,24 @@ var services = [
         'Lorem ipsum dolor sit amet con. Dolorem, molestias!'
     ]
 ];
+
+var testimonials = [
+    {
+        avatar: 'img/testimonials/face1.png',
+        p: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur soluta dignissimos quos perspiciatis illum hic voluptatem aut dolores atque maiores, iste obcaecati eligendi sit nisi dolorem velit provident asperiores harum.',
+        name: 'MS. Mandy Doe',
+        position: 'Founder of Winrawr'
+    },
+    {
+        avatar: 'img/testimonials/face2.png',
+        p: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur soluta dignissimos quos perspiciatis illum hic voluptatem aut dolores atque maiores, iste obcaecati eligendi sit nisi dolorem velit provident asperiores harum.',
+        name: 'MS. Mandy Foe',
+        position: 'Founder of OwO'
+    },
+    {
+        avatar: 'img/testimonials/face3.png',
+        p: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur soluta dignissimos quos perspiciatis illum hic voluptatem aut dolores atque maiores, iste obcaecati eligendi sit nisi dolorem velit provident asperiores harum.',
+        name: 'MS. Mandy Bam',
+        position: 'Founder of Zip'
+    }
+]
