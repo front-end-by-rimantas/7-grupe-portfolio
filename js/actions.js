5b173a3aa242611f120b6441ddbe3ff3f7296440
Rimantas <belauzas@yahoo.com>
2019-05-08 15:02:00 +0300

    Blog structure

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,6 @@
 "use strict";
 
 document.querySelector('#services .service-list').innerHTML = generateServices( services );
+
+
+document.querySelector('#blog .blog-list').innerHTML = generateBlog( blog );
--- js/function.js
+++ js/function.js
@@ -1,8 +1,7 @@
 "use strict";
 
 function generateServices( data ) {
-    var HTML = '',
-        key = 'icon';
+    var HTML = ''
 
     if ( !Array.isArray(data) ) {
         return HTML;
@@ -14,6 +13,27 @@ function generateServices( data ) {
                     <i class="fa fa-'+data[i].icon+'"></i>\
                     <h3>'+data[i].title+'</h3>\
                     <p>'+data[i].p+'</p>\
+                </div>';
+    }
+
+    return HTML;
+}
+
+function generateBlog( data ) {
+    var HTML = '';
+
+    if ( !Array.isArray(data) ) {
+        return HTML;
+    }
+
+    // konstravimas...
+    for ( var i=0; i<data.length; i++ ) {
+        HTML += '<div class="blog">\
+                    <div class="img"></div>\
+                    <h3>Web design</h3>\
+                    <div class="date">Posted on 25th Oct 2018</div>\
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, molestias...</p>\
+                    <a href="#" class="btn btn-red">Read more</a>\
                 </div>';
     }
 
